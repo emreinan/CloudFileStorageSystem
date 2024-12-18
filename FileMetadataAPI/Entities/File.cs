20d9add9479ad85f5f73a3391f54c0e09ba472c1
Emre İnan <emreinannn@gmail.com>
2024-12-19 00:54:15 +0300

    feat: User, File, FileShare entites created.

---
+++ FileMetadataAPI/Entities/File.cs
@@ -0,0 +1,10 @@
+﻿namespace FileMetadataAPI.Entities;
+
+public class File
+{
+    public int Id { get; set; }
+    public int OwnerId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime UploadDate { get; set; }
+}
