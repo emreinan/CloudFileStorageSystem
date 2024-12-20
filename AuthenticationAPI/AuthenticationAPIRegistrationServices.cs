342ef9b998a0f317ca4e8630ae18d25f8c090d20
Emre İnan <emreinannn@gmail.com>
2024-12-20 23:42:37 +0300

    feat: SwaggerGen injected to AuthAPI.

--- AuthenticationAPI/AuthenticationAPIRegistrationServices.cs
+++ AuthenticationAPI/AuthenticationAPIRegistrationServices.cs
@@ -3,6 +3,7 @@ using AuthenticationAPI.Application.Services.Auth;
 using AuthenticationAPI.Persistence.Context;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using System.Reflection;
 
 namespace AuthenticationAPI;
@@ -21,7 +22,42 @@ public static class AuthenticationAPIRegistrationServices
         {
             configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
         });
+        AddSwaggerGen(services);
 
         return services;
+    }
+
+    private static void AddSwaggerGen(IServiceCollection services)
+    {
+        services.AddSwaggerGen(c =>
+        {
+            c.AddSecurityDefinition(
+                name: "Bearer",
+                securityScheme: new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description =
+                        "JWT Authorization header using the Bearer scheme. " +
+                        "\r\n\r\n Enter 'Bearer' [space] and then your token in the text input below." +
+                        "\r\n\r\nExample: \"Bearer 12345.54321\""
+                }
+            );
+            c.AddSecurityRequirement(
+                new OpenApiSecurityRequirement
+                {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
+                },
+                new string[] { }
+            }
+                }
+            );
+        });
     }
 }
