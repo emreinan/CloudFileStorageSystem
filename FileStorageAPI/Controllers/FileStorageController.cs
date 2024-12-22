f80c9a47e50148d3fb217ec0cdfad7967d4f3af6
Emre İnan <emreinannn@gmail.com>
2024-12-22 18:06:52 +0300

    feat: FileStorageAPI Upload aciton and command implemented.

---
+++ FileStorageAPI/Controllers/FileStorageController.cs
@@ -0,0 +1,21 @@
+﻿using FileStorageAPI.Application.Features.Commands.Upload;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileStorageAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FileStorageController(IMediator mediator) : ControllerBase
+{
+    [HttpPost("upload")]
+    public async Task<IActionResult> Upload([FromForm] UploadFileDto uploadFileDto)
+    {
+        var command = new UploadFileCommand { Upload = uploadFileDto };
+        var result = await mediator.Send(command);
+        return Ok(result);
+    }
+
+    
+}
