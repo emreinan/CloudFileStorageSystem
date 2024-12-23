f3f6690cfdd5fe94e94d9c6c3389b980a3172e96
Emre İnan <emreinannn@gmail.com>
2024-12-23 21:57:45 +0300

    feat: FileMetadataAPI GetFiles and GetByIdFile queries implemented.

---
+++ FileMetadataAPI/Application/Features/Queries/GetById/GetByIdFileQuery.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using FileMetadataAPI.Application.Features.Rules;
+using FileMetadataAPI.Infrastructure.Context;
+using MediatR;
+
+namespace FileMetadataAPI.Application.Features.Queries.GetById;
+
+public class GetByIdFileQuery : IRequest<GetByIdFileQueryDto>
+{
+    public int Id { get; set; }
+
+    class GetByIdFileQueryHandler(FileMetaDataDbContext fileMetaDataDbContext, IMapper mapper, FileBusinessRules fileBusinessRules) : IRequestHandler<GetByIdFileQuery, GetByIdFileQueryDto>
+    {
+        public async Task<GetByIdFileQueryDto> Handle(GetByIdFileQuery request, CancellationToken cancellationToken)
+        {
+            var file = await fileMetaDataDbContext.Files.FindAsync(request.Id);
+            await fileBusinessRules.FileIsExists(file);
+            return mapper.Map<GetByIdFileQueryDto>(file);
+        }
+    }
+
+}
---
+++ FileMetadataAPI/Application/Features/Rules/FileBusinessRules.cs
@@ -0,0 +1,15 @@
+﻿using FileMetadataAPI.Infrastructure.Context;
+using File= FileMetadataAPI.Domain.Entities.File;
+
+namespace FileMetadataAPI.Application.Features.Rules;
+
+public class FileBusinessRules(FileMetaDataDbContext fileMetaDataDbContext)
+{
+    public async Task FileIsExists(File? file)
+    {
+        if (file is null)
+        {
+            throw new Exception("File not found");
+        }
+    }
+}
