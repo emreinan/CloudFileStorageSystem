c8f69b72af3dced94b0c3aa85bfe8f460523188b
Emre İnan <emreinannn@gmail.com>
2024-12-24 19:22:46 +0300

    feat: GatewayAPI implemented ReverseProxy.

---
+++ GatewayAPI/GatewayAPIRegistrationServices.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+namespace GatewayAPI
+{
+    public static class GatewayAPIRegistrationServices
+    {
+        public static IServiceCollection RegisterServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            AddJwtAuthentication(services, configuration);
+            AddSwaggerGen(services);
+            
+            return services;
+        }
+        private static void AddJwtAuthentication(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+               .AddJwtBearer(options =>
+               {
+                   options.TokenValidationParameters = new TokenValidationParameters
+                   {
+                       ValidateIssuer = true,
+                       ValidateAudience = true,
+                       ValidateLifetime = true,
+                       ValidIssuer = configuration["TokenOptions:Issuer"],
+                       ValidAudience = configuration["TokenOptions:Audience"],
+                       ValidateIssuerSigningKey = true,
+                       IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["TokenOptions:SecurityKey"]))
+                   };
+               });
+        }
+
+        private static void AddSwaggerGen(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition(
+                    name: "Bearer",
+                    securityScheme: new OpenApiSecurityScheme
+                    {
+                        Name = "Authorization",
+                        Type = SecuritySchemeType.ApiKey,
+                        Scheme = "Bearer",
+                        BearerFormat = "JWT",
+                        In = ParameterLocation.Header,
+                        Description =
+                            "JWT Authorization header using the Bearer scheme. " +
+                            "\r\n\r\n Enter 'Bearer' [space] and then your token in the text input below." +
+                            "\r\n\r\nExample: \"Bearer 12345.54321\""
+                    }
+                );
+                c.AddSecurityRequirement(
+                    new OpenApiSecurityRequirement
+                    {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
+                },
+                new string[] { }
+            }
+                    }
+                );
+            });
+        }
+    }
+}
