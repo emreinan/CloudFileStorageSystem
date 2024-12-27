e1e18f84692836a8dd9d81c1c94aef6c65d83420
Emre İnan <emreinannn@gmail.com>
2024-12-27 17:39:35 +0300

    fixed: Error Model, View, Action fixed.

--- CloudFileStorageMVC/Controllers/HomeController.cs
+++ CloudFileStorageMVC/Controllers/HomeController.cs
@@ -26,7 +26,16 @@ namespace CloudFileStorageMVC.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var errorViewModel = new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier,
+                Title = TempData["ErrorTitle"]?.ToString() ?? "An error occurred",
+                Message = TempData["ErrorMessage"]?.ToString() ?? "An unexpected error occurred while processing your request.",
+                StatusCode = TempData["ErrorCode"] != null ? int.Parse(TempData["ErrorCode"].ToString()!) : 500
+            };
+
+            return View(errorViewModel);
         }
+
     }
 }
